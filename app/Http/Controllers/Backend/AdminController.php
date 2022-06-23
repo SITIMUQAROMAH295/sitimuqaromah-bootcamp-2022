c54efe2af3df8da3339f80aafc1486b74cd46592
sitimuqaromah <sitimomoy62@gmail.com>
2022-06-23 14:04:52 +0700

    first commit

---
+++ app/Http/Controllers/Backend/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+    public function getIndex() {
+        return view('backend.page.dashboard.index');
+    }
+}
+
---
+++ resources/views/template/front/head.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <meta name="author"content="" />
+    <title>USM Shop</title>
+    <Link rel="icon" type="image/x-icon" href="{{asset('assets/favicon.ico')}}" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <Link href="{{asset('assets/css/styles.css')}}" rel="stylesheet" />
+</head>
+<body>
+    
+</body>
+</html>
